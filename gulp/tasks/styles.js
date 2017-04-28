ae78ef693c2fe8c05181371c00f7eba25e888ffc
Bill <billbakas@Vasileioss-MBP.lan>
2017-04-28 17:46:13 +0300

    Completed Automatic Sprite Generation

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -6,9 +6,9 @@ nested = require('postcss-nested'),
 cssImport = require('postcss-import'),
 mixins = require('postcss-mixins');
 
-gulp.task('styles', function(){
-    return gulp.src('./app/assets/styles/styles.css')
-    .pipe(postcss([cssImport,mixins,cssvars,nested,autoprefixer]))
+gulp.task('styles', function() {
+  return gulp.src('./app/assets/styles/styles.css')
+    .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
     .on('error', function(errorInfo) {
       console.log(errorInfo.toString());
       this.emit('end');
