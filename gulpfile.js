85f58057792e9762e91e7166d6c86c2c8834347b
Bill <billbakas@Vasileioss-MBP.lan>
2017-05-03 15:39:48 +0300

    final touches

--- gulpfile.js
+++ gulpfile.js
@@ -2,3 +2,12 @@ require('./gulp/tasks/styles');
 require('./gulp/tasks/watch');
 require('./gulp/tasks/sprites');
 require('./gulp/tasks/scripts');
+require('./gulp/tasks/build');
+
+var gulp = require('gulp');
+var ghPages = require('gulp-gh-pages');
+
+gulp.task('deploy', function() {
+  return gulp.src('./dist/**/*')
+    .pipe(ghPages());
+});
