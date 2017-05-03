49c88111237a9e1cdd2bf951ad2130bd0972a052
Bill <billbakas@Vasileioss-MBP.lan>
2017-05-03 15:55:26 +0300

    Added docs folder.

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,6 @@ var gulp = require('gulp');
 var ghPages = require('gulp-gh-pages');
 
 gulp.task('deploy', function() {
-  return gulp.src('./dist/**/*')
+  return gulp.src('./docs/**/*')
     .pipe(ghPages());
 });
